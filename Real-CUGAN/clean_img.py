e4830d02fe6b52b7b1810b8eb650b2a99fa94c06
Yokohide0317 <yokohide0317@gmail.com>
2022-02-19 12:55:27 +0900

    comp

--- Real-CUGAN/clean_img.py
+++ Real-CUGAN/clean_img.py
@@ -1,29 +1,33 @@
 import os
 import sys
-sys.path.append(".")
+#root_dir = "/Users/yokoda/workspace/yokoscripts/clean_img/"
+#sys.path.append(root_dir+"Real-CUGAN")
+sys.path.append("./")
 import cv2
 from upcunet_v3 import RealWaifuUpScaler
 from time import time as ttime
 
-ModelPath="./model/up2x-latest-no-denoise.pth"
-Tile=4
 
-def main(file_path):
-    file_name = file_path.split("/")[-1]
-    img_name = file_name.split(".")[0]
-    file_type = file_name.split(".")[-1]
-    
-    upscaler = RealWaifuUpScaler(2, ModelPath, half=False, device="cpu")
+def main(file_name):
+    upscaler = RealWaifuUpScaler(2, f"./model/up2x-latest-no-denoise.pth", half=False, device="cpu")
+    Tile = 4
+    Amplification = 2
+
+    t0 = ttime()
     try:
-        img = cv2.imread(file_path)[:, :, [2, 1, 0]]
+        img = cv2.imread(file_name)[:, :, [2, 1, 0]]
         result = upscaler(img,tile_mode=2)
-        cv2.imwrite(f"./{img_name}_cleaned.{file_type}",result[:, :, ::-1])
+        output_name = ".".join(file_name.split(".")[:-1]) + "_cleaned." + file_name.split(".")[-1]
+        cv2.imwrite(output_name, result[:, :, ::-1])
     except RuntimeError as e:
-        print("Failed")
-        print(e)
+        print ("Failed...")
+        print (e)
     else:
         print("Done")
+    t1 = ttime()
+    print("Compleated", t1 - t0)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
-
+    file_name = sys.argv[1]
+    print(file_name)
+    main(file_name)
