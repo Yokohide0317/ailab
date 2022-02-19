1facbca7bfda68475b3bde455e8887fe42197bce
Yokohide0317 <yokohide0317@gmail.com>
2022-02-19 18:16:34 +0900

    unar to unzip

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 wget https://github.com/bilibili/ailab/releases/download/Real-CUGAN/updated_weights.zip
-unar updated_weights.zip
+unzip updated_weights.zip
 rm updated_weights.zip
 mkdir -p src/model
 mv updated_weights/up2x-latest-no-denoise.pth src/model
